91b0322e08a7636c3eb8f110a6631a9863b264cc
subin <subini1707@khu.ac.kr>
2020-12-04 17:51:49 +0900

    spring.js 이미지 출력 수정

--- front/src/spring.js
+++ front/src/spring.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './spring.css?ver=1.3';
+import './spring.css?ver=1.4';
 import spring from './season/spring.jpg';
 
 const Spring = ( { history } ) => 
 {
     return (
-        <div className='spring'>
-            <img src = {spring} alt = "spring"/>
+        <div className='spring_class'>
+            <img src = {spring} width='1300' height='600' alt = "spring"/>
             <div id="btn_back">
             <button id="back_btn" onClick={ () => {history.push("/")}}>back</button>
             </div>
